f4f82259eec679856fcdf803aac578b1c48522b1
wherculano <wagherculano@hotmail.com>
2020-10-24 22:26:42 -0300

    Projeto refeito

--- igtiweb/urls.py
+++ igtiweb/urls.py
@@ -1,4 +1,4 @@
-"""igtiflix URL Configuration
+"""igtiweb URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('principal/', view=include('principal.urls'))
+    path('principal/', include('principal.urls')),
 ]
--- igtiweb/wsgi.py
+++ igtiweb/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for igtiflix project.
+WSGI config for igtiweb project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'igtiflix.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'igtiweb.settings')
 
 application = get_wsgi_application()
