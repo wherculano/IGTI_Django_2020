2f85e689de735da12e219f017cd6b834695a9512
wherculano <wagherculano@hotmail.com>
2020-10-25 11:23:09 -0300

    Corrigido o caminho da app

--- igtiweb/urls.py
+++ igtiweb/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('principal/', include('principal.urls')),
+    path('principal/', include('igtiweb.principal.urls')),
 ]
