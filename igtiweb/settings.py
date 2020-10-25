4cb740ef572f9d78f6cf8f1806fd28dda35659f6
wherculano <wagherculano@hotmail.com>
2020-10-25 12:23:18 -0300

    Adicionados arquivos estaticos

--- igtiweb/settings.py
+++ igtiweb/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_DIR = BASE_DIR/'igtiweb/templates'
+STATIC_DIR = BASE_DIR/'igtiweb/static'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'igtiweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES = [
-    BASE_DIR/'static'
+STATICFILES_DIR = [
+    STATIC_DIR,
 ]
