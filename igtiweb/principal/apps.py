b7c3bb4df48a0a10848f87906d707eefa149d1a4
wagner <wagner.santos@accurate.com.br>
2022-04-23 23:11:24 -0300

    Updated files

--- igtiweb/principal/apps.py
+++ igtiweb/principal/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class IgtiflixConfig(AppConfig):
-    name = 'principal'
+    name = 'igtiweb.principal'
--- igtiweb/serie/apps.py
+++ igtiweb/serie/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class SerieConfig(AppConfig):
-    name = 'serie'
+    name = 'igtiweb.serie'
